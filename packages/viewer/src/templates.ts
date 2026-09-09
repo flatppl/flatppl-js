6c5ab28c2e74981b4112e125a7f6a4c77f135ada
Oliver Schulz <oschulz@mpp.mpg.de>
2026-09-09 16:34:21 +0200

    viewer: the gesture hint covers the math pane's equations too
    Assisted-By: generative AI

--- packages/viewer/src/templates.ts
+++ packages/viewer/src/templates.ts
@@ -477,7 +477,7 @@ export var VIEWER_BODY_HTML = `
 </div>
 <div id="tooltip"></div>
 <div id="info">
-<span class="hint">Click a node to see details &middot; double-click to drill down &middot; Ctrl+click to jump to source &middot; click &#8862;/&#8863; or Shift+click an anchor to collapse/expand that group</span>
+<span class="hint">Click a node or equation to see details <span class="hint">Click a node to see details &middot; double-click to drill down &middot; Ctrl+click to jump to sourcemiddot; double-click to drill down <span class="hint">Click a node to see details &middot; double-click to drill down &middot; Ctrl+click to jump to sourcemiddot; Ctrl+click to jump to source &middot; click &#8862;/&#8863; or Shift+click an anchor to collapse/expand that group</span>
 </div>
 `;
 
